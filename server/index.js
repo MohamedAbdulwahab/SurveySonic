30a58b549bea642c0572b8f248336eeda325179c
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-05-17 13:18:35 -0700

    completed backend authentication with Google OAuth

--- server/index.js
+++ server/index.js
@@ -5,11 +5,34 @@ const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+/* keys */
+const keys = require('./config/keys');
+
 // assign express to app (community convention).
 const app = express();
 
 // use passport's GoogleStrategy
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT);
